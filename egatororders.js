481386c75fe6411aa07bee2ac0f5fc2164a6607a
BunmiOpcode <bellobunmi123@gmail.com>
2022-11-19 03:34:52 -0700

    first commit

---
+++ egatororders.js
@@ -0,0 +1,44 @@
+const Orders = [
+  {
+    productName: 'Foldable Mini Drone',
+    productNumber: '85631',
+    paymentStatus: 'Due',
+    Shipping: 'Pending'
+  },
+  {
+    productName: 'Larvendar KF102 Drone',
+    productNumber: '36378',
+    paymentStatus: 'Refunded',
+    Shipping: 'Declined'
+  },
+  {
+    productName: 'Ruko F11 Pro Drone',
+    productNumber: '493471',
+    paymentStatus: 'Due',
+    Shipping: 'Pending'
+  },
+  {
+    productName: 'Drone with camera Drone',
+    productNumber: '96996',
+    paymentStatus: 'paid',
+    Shipping: 'Delivered'
+  },
+  {
+    productName: 'GPS 4K Drone',
+    productNumber: '22821',
+    paymentStatus: 'Paid',
+    Shipping: 'Delivered'
+  },
+  {
+    productName: 'DJI Air 25',
+    productNumber: '81475',
+    paymentStatus: 'Due',
+    Shipping: 'Pending'
+  },
+  {
+    productName: 'Lozengo Drone',
+    productNumber: '00482',
+    paymentStatus: 'Paid',
+    Shipping: 'Delivered'
+  },
+]
